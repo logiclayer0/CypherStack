07d1a076e2daab306fe0a24756c24503e5c0a0c0
tria <tashubajpai160@gmail.com>
2026-09-10 17:47:58 +0530

    Update TrustMap.tsx

--- src/components/TrustMap.tsx
+++ src/components/TrustMap.tsx
@@ -130,7 +130,7 @@ function TrustMap({ nodes, links, height = 500 }: Props) {
           .duration(150)
           .attr('r', (dd: any) => 20 + dd.score / 8)
       })
-      .on('mouseleave', function (_event: any) {
+      .on('mouseleave', function (_, d: any) {
         setHovered(null)
         d3.select(this)
           .select('circle')
